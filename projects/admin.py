9eac1d0314f6994329403ba799d5f347763506bf
raghav1010 <raghavkol1997@gmail.com>
2024-03-22 22:57:01 +0530

    v-1.0.1: devsearch base requirement implementation.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Project, Review, Tag
+
+admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Tag)
