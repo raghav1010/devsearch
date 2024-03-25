b054b1d3824f30d3b04580d79ff9dd67185b070b
raghav1010 <raghavkol1997@gmail.com>
2024-03-25 14:03:16 +0530

    v-1.0.2: docker, envs setup along with code restructure.

---
+++ core_apps/users/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core_apps.users'
+
+    def ready(self):
+        pass
--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,29 +1,17 @@
-"""
-URL configuration for devsearch project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
+    path(settings.ADMIN_URL, admin.site.urls),
+    path('', include('core_apps.projects.urls')),
 
-    path('', include('users.urls')),
+    path('', include('core_apps.users.urls')),
+
+    path('', include('core_apps.notifications.urls')),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
          name="reset_password"),
